7e550fdea110214545a48b4aa6218c0c38a66048
fikrijava <email_fatwaalfiqri@gmail.com>
2026-04-28 14:29:21 +0700

    Menyelesaikan fitur kontak dan profil

---
+++ web_lanjut2026/resources/views/kontak.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <a href="/home">Home</a>
+    <a href="/kontak">Kontak</a>
+    <a href="/">Profil</a>
+
+    <h1>Halaman Kontak</h1>
+</div>
---
+++ web_lanjut2026/resources/views/profil.blade.php
@@ -0,0 +1,8 @@
+<div>
+
+    <a href="/home">Home</a>
+    <a href="/kontak">Kontak</a>
+    <a href="/">Profil</a>
+
+    <h1>Halaman Profil</h1>
+</div>
